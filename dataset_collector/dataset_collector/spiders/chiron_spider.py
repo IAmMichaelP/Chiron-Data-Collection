41c48c52e265d5bbf618a78a60cbf2f79a41882e
IAmMichaelP <110220534+IAmMichaelP@users.noreply.github.com>
2024-10-04 10:38:19 +0800

    initiated SplashRequest

--- dataset_collector/dataset_collector/spiders/chiron_spider.py
+++ dataset_collector/dataset_collector/spiders/chiron_spider.py
@@ -1,20 +1,21 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-
+from scrapy_splash import SplashRequest
+# fetch('http://localhost:8050/render.html?url=https://www.gmanetwork.com/news/lifestyle/healthandwellness/922501/phasing-out-teen-smoking-could-save-1-2-million-lives-who-cancer-agency-study/story/')
 
 class CrawlingSpider(CrawlSpider):
     name = "chironCrawler"
-    allowed_domains = ["abs-cbn.com"]
-    start_urls = ["https://news.abs-cbn.com/"]
-
-    rules = (
-        Rule(LinkExtractor(allow="lifestyle/health-wellness/*")),
-        # Rule(LinkExtractor(allow="catalogue", deny="category"), callback="parse_item"), 
-    )
-
-    def parse_item(self, response):
+    
+    # S
+    # 
+    tart requests method
+    def start_requests(self):
+        url = 'https://news.abs-cbn.com/lifestyle/health-wellness/2024/10/3/pink-crossing-in-celebration-of-breast-cancer-awareness-month-1433'
+        yield SplashRequest(url=url, callback=self.parse)
+    
+    # Parsing the response
+    def parse(self, response):
         yield {
-            "body": response.css("#bodyTopPart h5 div p::text").get(),
-            # "price": response.css(".price_color::text").get(),
-            # "availability": response.css(".availability::text")[1].get().strip(),
+            'title': response.css('.css-u1oaus h2::texscrt').get(),  # Adjust this selector if needed
+            'body': response.css('#bodyTopPart p::text').getall(),  # Adjust this selector if needed
         }
