44dcbf625b5e9c694b05210a505ad1c9b58b844f
IAmMichaelP <110220534+IAmMichaelP@users.noreply.github.com>
2024-10-21 09:37:10 +0800

    trying to update

--- dataset_collector/dataset_collector/spiders/philstar_spider.py
+++ dataset_collector/dataset_collector/spiders/philstar_spider.py
@@ -16,3 +16,8 @@ class CrawlingSpider(CrawlSpider):
         # Rule(LinkExtractor(allow=r"lifestyle/healthandwellness/\d+/.+/story/"), callback="parse_item", follow=True),
         
     )
+    def parse_item(self, response):
+        yield {
+            'title': response.css('.article__title h1::text').get(),
+            # 'body': response.css('div.article__writeup p::text, div.article__writeup div::text').getall(),  # Extract text from both <p> and <div> inside article__writeup
+        }
