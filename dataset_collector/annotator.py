c1dcb84a1658b5950501251028507d1273023807
Miki <110220534+IAmMichaelP@users.noreply.github.com>
2025-03-05 23:02:19 +0800

    scraped links through puppeteer

--- dataset_collector/annotator.py
+++ dataset_collector/annotator.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Step 1: Read the CSV file using pandas
-df = pd.read_csv(r"C:\Users\agaro\Documents\GitHub\Chiron\dataset_collector\dataset4.csv")
+df = pd.read_csv(r"C:\Users\agaro\Documents\GitHub\Chiron\dataset_collector\dataset3.csv")
 
 # Step 2: Add 'annotation' column with a value of 0 for each row
-df['annotation'] = 1
+df['annotation'] = 0
 
 # Step 3: Write the updated DataFrame to a CSV file
-df.to_csv('annotated_dataset4.csv', index=False)
+df.to_csv('natural_news.csv', index=False)
 # df.to_csv('annotated_gma.csv', mode='a', index=False, header=False)
 
 print("Data processing complete. Check 'chiron.csv' for the results.")
--- puppeteer-collector/annotator.py
+++ puppeteer-collector/annotator.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Step 1: Read the CSV file using pandas
-df = pd.read_csv(r"C:\Users\agaro\Documents\GitHub\Chiron\puppeteer-collector\consolidated.csv")
+df = pd.read_csv(r"C:\Users\agaro\Documents\GitHub\Chiron\puppeteer-collector\WebMD\scraped_data.csv")
 
 # Step 2: Add 'annotation' column with a value of 0 for each row
 df['annotation'] = 1
 
 # Step 3: Write the updated DataFrame to a CSV file
-df.to_csv('annotated_dataset4.csv', index=False)
+df.to_csv('webMD.csv', index=False)
 # df.to_csv('annotated_gma.csv', mode='a', index=False, header=False)
 
 print("Data processing complete. Check 'chiron.csv' for the results.")
